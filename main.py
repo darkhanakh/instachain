a5cc944a86dd4ffea12f3a267e9fcce4ad0efb14
Darkhan Akhmetov <darkhanahmetov228@gmail.com>
2025-06-24 23:19:34 +0500

    refactor: streamline main.py for Instagram login functionality and remove unused project files

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import instaloader
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+L = instaloader.Instaloader()
 
+USER = input("Enter username: ")
+PASSWORD = input("Enter password: ")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+L.login(USER, PASSWORD)
